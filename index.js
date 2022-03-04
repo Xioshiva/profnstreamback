c3ee05bd8a7b8ef7406bfa6511c25a1107e1def9
Antoine Blancy <antoine.blancy@etu.hesge.ch>
2022-03-04 16:04:44 +0100

    Pretty much done with the server side webrtc broacsting stuff

--- index.js
+++ index.js
@@ -17,12 +17,64 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
 });
 
+
+let connections = {};
+
+const config = {
+  iceServers: [
+    {
+      urls: ["stun:stun.l.google.com:19302"]
+    }
+  ]
+};
+
+const constraints = {
+  video: { facingMode: "user" }
+  // Uncomment to enable audio
+  // audio: true,
+};
+
 io.on('connection', (socket) => {
   console.log("Connected!");
   socket.on('init', (roomID) => {
     console.log("joined to " + roomID);
     socket.join(roomID);
+
+    //Initiate webRTC p2p connection
+    const con = new RTCPeerConnection(config);
+    connections[socket] = con;
+
+    let stream = "AH"; //insert data;
+    stream.getTracks().forEach(track => con.addTrack(track, stream));
+
+    con.onicecandidate = event => {
+      if(event.candidate){
+        socket.emit("candidate", socket, event.candidate);
+      }
+    }
+
+    con
+      .createOffer()
+      .then(sdp => con.setLocalDescription(sdp))
+      .then(() =>{
+        socket.emit("offer", id, con.localDescription)
+      });
   });
+
+  socket.on('answer', (socket, descr)=>{
+    con[id].setRemoteDescription(descr);
+  });
+
+  socket.on("candidate", (socket, candidate) =>{
+    connections[socket].addIceCandidate(new RTCIceCandidate(candidate));
+  });
+
+
+  server.on("close", socket =>{
+    connections[socket].close();
+    delete connections[socket];
+  });
+
 
   socket.on('chat message', (args)  => {
     console.log("msg: " + args['msg'] + " roomID: " + args['roomID'] + " userID: " + args['userID']);
@@ -41,4 +93,5 @@ io.on('connection', (socket) => {
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
+
 
