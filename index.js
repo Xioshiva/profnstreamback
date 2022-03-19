0a0df98e0041bab51cf183f18db9ca1ab4c93865
dorian.bernasco <dorian.bernasco@etu.hesge.ch>
2022-03-20 00:50:31 +0100

    timer redis

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 
-
-const redis = require('redis')
 const fs = require('fs');
 const bodyParser = require('body-parser');
 
@@ -15,11 +13,14 @@ app.use(bodyParser.urlencoded({
 }));
 
 
+var redis = require('redis');
+
+
+
 var socketList = {};
 var timerList = [];
 
 const cachePath = './cache.json'
-
 
 const { Server } = require("socket.io");
 const io = require("socket.io")(server, {
@@ -31,7 +32,7 @@ const io = require("socket.io")(server, {
 });
 
 
-const TRIALTIMEDURATION = 20 * 1000;
+const TRIALTIMEDURATION = 20 * 10;
 
 
 class UserStreamEntity {
@@ -40,7 +41,7 @@ class UserStreamEntity {
       this.streamId = streamId;
       this.time = time;
       this.timer = new timer(function() {
-        //console.log("FINISH");
+        console.log("FINISH");
         callback.call()
 
         //eraseCache(userId,streamId)
@@ -62,7 +63,11 @@ function timer(callback, delay) {
     this.start = function() {
         running = true
         started = new Date()
-        id = setTimeout(callback, remaining)
+        console.log(delay)
+        if(delay >= 0){
+          id = setTimeout(callback, remaining)
+        }
+        
     }
 
     this.pause = function() {
@@ -86,297 +91,173 @@ function timer(callback, delay) {
     
 }
 
-function existCache(userId,streamId){
-    var obj = {
-        table: []
-     };
-
-     var res = 0
-     fs.readFile(cachePath, 'utf8', function readFileCallback(err, data){
-     if (err){
-        console.log(err);
-     } 
-     else {
-        obj = JSON.parse(data); //now it an object
-        res = obj.table.findIndex(i => { return i.userId === userId && i.streamId === streamId});
-    }});
-    //console.log("res")
-    //console.log(res)
-    return (res != -1 && typeof res != 'undefined')
-
-}
-
-// Récupère le temps restant d'un utilisateur pour un stream donné
-async function getTimeFromCache(userId, streamId){
-    var obj = {
-        table: []
-     };
-
-    try{
-        let data = fs.readFileSync(cachePath, 'utf8');
-        obj = JSON.parse(data); //now it an object
-        console.log(obj)
-        return obj.table[obj.table.findIndex(i => {
-            return i.userId == userId && i.streamId == streamId
-          })].time; //del some data
-    }catch{
-        console.log(err);
-    }
-
-}
 
 
-// newEntry : UserStreamEntity 
-function addTimer(newEntry){
-  timerList.push(newEntry);
-  writeCache(newEntry.userId,newEntry.streamId,newEntry.timer.getTimeLeft())
-}
-/*
-app.post('/cache', async (request, response) => {
-    
-    if(request.query.userId != undefined){
-        userId = request.query.userId
-        streamId = request.query.streamId
-        time = request.query.time
-
-        response.status(200).json(response.json);
-    } else{
-        response.status(400).end("Failed to get the ressource");
-    }
-});
-*/
-/*
-function updateCache(){
-    
-    f.readFile(cachePath, 'utf8', function readFileCallback(err, data){
-      if (err){
-          console.log(err);
-      } 
-      else {
-            timerList.forEach(element => {
-            
-        })
-    }});
-
-}*/
-
-function eraseCache(userId,streamId){
-    
-    var obj = {
-        table: []
-     };
-     
-    fs.readFile(cachePath, 'utf8', function readFileCallback(err, data){
-        if (err){
-            console.log(err);
-        } else {
-            console.log("earse");
-            console.log("##############################");
-        obj = JSON.parse(data); //now it an object
-        console.log("Reading:")
-        console.log(obj)
-        fs.unlinkSync('cache.json');
-            
-        delete obj.table[obj.table.findIndex(i => {return i.userId == userId && i.streamId == streamId})]; //del some data
-        obj.table = obj.table.filter((a) => a)
-        console.log("Writing: ")
-        json = JSON.stringify(obj); //convert it back to json
-        console.log(json);
-        fs.writeFile(cachePath, json, 'utf8', function(error) {
-            //console.log(json);
-            console.log("File has been erased");
-        }); // write it back 
-        console.log("##############################");
-    }});
-}
-
-function writeCache(userId,streamId,time) {
-
-    var entity = {
-        "userId": userId,
-        "streamId": streamId,
-        "time": time
-    }
-
-    var obj = {
-        table: []
-     };
-    
-    try {
-        var fs = require('fs');
-        if (fs.existsSync(cachePath)) {
-            fs.readFile(cachePath, 'utf8', function readFileCallback(err, data){
-                if (err){
-                    console.log(err);
-                } else {
-                    console.log("write")
-                    console.log("=============================");
-                    obj = JSON.parse(data); //now it an object
-
-                    
-                    console.log("ERASE:");
-                    delete obj.table[obj.table.findIndex(i => {return i.userId == userId && i.streamId == streamId})]; //del some data
-                    console.log(obj)
-                    obj.table = obj.table.filter((a) => a)
-                    console.log("Reading:");
-                    console.log(obj)
-                    obj.table.push(entity); //add some data
-                    json = JSON.stringify(obj); //convert it back to json
-                    console.log("Writing:");
-                    console.log(json)
-                    fs.writeFile(cachePath, json, 'utf8', function(error) {
-                    //console.log(json)
-                    console.log("Wrote in cache!");
-                    console.log("=============================");
-                  }); // write it back 
-            }});
-
-        }else{          
-
-
-            obj.table.push(entity)
-            var json = JSON.stringify(obj);
-            fs.writeFile(cachePath, json, 'utf8', function(error) {
-                console.log("Cache has been created");
-              })    
-        }
-    } catch(err) {
-        console.error(err)
-    }
-}
-
-
-
-function printCache(){
-    var obj = {
-        table: []
-     };
-    
-    try {
-        var fs = require('fs');
-        if (fs.existsSync(cachePath)) {
-            fs.readFile(cachePath, 'utf8', function readFileCallback(err, data){
-                if (err){
-                    console.log(err);
-                } else {
-                    obj = JSON.parse(data); //now it an object
-                    //console.log(obj)
-            }});
-
-        }    
-    }catch{
-        console.log(err)
-    }
-}
-
-function updateRemainingTime(userID, streamID){
-    var timerLeft = socketList[userID + "|" + streamID].timer.getTimeLeft();
-  
-    var entity = {
-        "userId": userID,
-        "streamId": streamID,
-        "time": timerLeft
-    }
-    console.log("ooooooooooooooooo");
-    //eraseCache(userID,streamID);
-    printCache();
-    console.log("TIME LEST")
-    console.log(timerLeft)
-    writeCache(userID,streamID,timerLeft);
-    printCache();
-    console.log("ooooooooooooooooo");
-}
-
-
-async function getRemainingTime(userID, streamID){
-    //If user-stream combo doesn´t exist in cache -> return TRIALTIMEDURATION
-    if(existCache(userID, streamID)){
-        console.log("ALREADY EXIIIIIIST ?")
-        //console.log("userID/streamID already exist in cache")
-        let remainingTime = await getTimeFromCache(userID, streamID);
-        console.log(remainingTime);
-        return remainingTime;
-    }else{
-        return TRIALTIMEDURATION;
-    }
-}
-
-app.listen(8080);
-console.log('Server started');
-
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});
 
 //This is called by the frontend whenever the page is loaded
 //in order to know the tie remaining
 app.get('/time/:stream /:user', (res,req) =>{
-    //Get the time remaining for the user. 
-    res.send(getRemainingTime(req.params.user, req.params.stream));
+  //Get the time remaining for the user. 
+  res.send(getRemainingTime(req.params.user, req.params.stream));
 });
 
 
+
+
+
+async function writeCache(userID,streamID,time) {
+
+  var value = userID + "|" + streamID
+  return add(value,time)
+}
+
+// Récupère le temps restant d'un utilisateur pour un stream donné
+async function getTimeFromCache(userID, streamID){
+  var value = userID + "|" + streamID;
+  return chope(value);
+}
+
+async function getRemainingTime(userID, streamID){
+    //If user-stream combo doesn´t exist in cache -> return TRIALTIMEDURATION
+    return existCache(userID + "|" + streamID).then(a=> {
+      if(a){
+        console.log("Get Remaining Time -> Si le timer UserID|StreamID Exist ")
+        //console.log("userID/streamID already exist in cache")
+        let remainingTime = getTimeFromCache(userID, streamID);
+        console.log("remaining time = " + remainingTime);
+        return remainingTime;
+    }else{
+
+      console.log("durée max")
+        return TRIALTIMEDURATION;
+    }
+
+    });
+
+}
+
+async function existCache(value){
+  var client = redis.createClient();
+  
+  return client.connect().then( a =>{
+    return client.exists(value);
+    });
+
+}
+
+async function add(key,timer) {
+
+  var client = redis.createClient();
+  return client.connect().then( a =>{
+
+      return client.set(key, timer).then(b=>
+        client.disconnect()
+      );
+
+    });
+
+  console.log("heu")
+}
+
+
+
+
+async function chope(key) {
+
+  var client = redis.createClient();
+  
+  return client.connect().then( a =>{
+    return client.get(key).then( b =>{
+      client.disconnect();
+      return b;
+    });
+    });
+}
+
+async function addTimer(newEntry){
+  //timerList.push(newEntry);
+  return writeCache(newEntry.userId,newEntry.streamId,newEntry.timer.getTimeLeft())
+}
+
+function updateCache(){
+
+}
 
 io.on('connection', (socket) => {
 
-  var roomID = "";
-  var userID = "";
-  //console.log("Connected!");
-  socket.on('init', (args) => {
-    roomID = args['roomID'];
-    userID = args['userID'];
-    //console.log("joined to " + args['roomID']);
-    socket.join(args['roomID']);
-    // key is userID + "|" + roomID
+    var roomID = "";
+    var userID = "";
 
-    let remainingTime = getRemainingTime(userID, roomID)
-
-    socketList[args['userID'] + "|" + args['roomID']] = new UserStreamEntity(args['userID'], args['roomID'],remainingTime, socket, i =>
-      {
-        //console.log("Done");
-        socket.disconnect();
-        //delete socketList[args['userID'] + "|" + args['roomID']];
-      });
-
-    //console.log("add new Timer")
-    if(!existCache(userID, roomID)){
-        //console.log("HELLO CACHE IS NOT EXIST");
-        addTimer(socketList[userID + "|" +roomID]);
-    }
-  });
-
-  socket.on('chat message', (args) => {
-    socket.to(args['roomID']).emit('recieve message', ({ "msg": args['msg'], "userID": args['userID'], "question": args['question'] }));
-  });
-
-  socket.on('disconnect', (reason) =>{
-      
-    //console.log(userID + "|" + roomID);
-    socketList[userID + "|" + roomID].timer.pause();
-    //console.log("Paused!");
-    //console.log(socketList);
-    updateRemainingTime(userID,roomID);
-
-    delete socketList[userID + "|" + roomID];
     
-    //Replace userID roomID : time by new time which is
+    socket.on('init', (args) => {
+      console.log("eheh")
+      roomID = args['roomID'];
+      userID = args['userID'];
+      socket.join(args['roomID']);
 
 
-    /*for (var key in socketList){
-        if(JSON.stringify([key].socket) === JSON.stringify(socket)){
-            socketList[key].pause()
-            console.log("Paused!");
+      getRemainingTime(userID, roomID).then( a => {
+
+        let remainingTime = a;
+        console.log("remaining : " + remainingTime)
+        
+        if(remainingTime <= 0){
+          socketList[args['userID'] + "|" + args['roomID']] = new UserStreamEntity(args['userID'], args['roomID'],remainingTime, socket, i =>
+          {
+            console.log("Timer Ended");
+            socket.disconnect();
+          });
+          console.log("Detected negative or 0 time!");
+          socket.disconnect();
+        }else{
+          socketList[args['userID'] + "|" + args['roomID']] = new UserStreamEntity(args['userID'], args['roomID'],remainingTime, socket, i =>
+          {
+            console.log("Timer Ended");
+            socket.disconnect();
+          });
+
+          console.log(socketList[userID + "|" +roomID]);
+          existCache(userID + "|"+ roomID).then(b => {      
+            if(b == 0){
+            addTimer(socketList[userID + "|" +roomID]);
+          }});
         }
-    }*/
-    socket.disconnect();
+        
+      })
+      
+
+      /*
+      if(!existCache(userID, roomID)){
+        console.log("SHIT")
+        addTimer(socketList[userID + "|" +roomID]);
+      }*/
+    });
+  
+    socket.on('chat message', (args) => {
+      socket.to(args['roomID']).emit('recieve message', ({ "msg": args['msg'], "userID": args['userID'], "question": args['question'] }));
+    });
+  
+    socket.on('disconnect', (reason) =>{
+      //socketList[userID + "|" + roomID].timer.pause();
+      console.log(socketList[userID + "|" + roomID])
+      addTimer(socketList[userID + "|" + roomID]).then( o => {
+        console.log("HELOO MY NAME IS CHIKCHIKASLIMSHADY")
+        console.log(socketList[userID + "|" + roomID])
+        delete socketList[userID + "|" + roomID]
+        console.log("dec")
+        socket.disconnect();
+      });
+      
+    });
+
   });
 
-  //console.log('a user connected');
-});
+app.listen(8080);
+console.log('Server started');
 
-//On connection to socket -> add the socket to the correct room
-
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+  
+  //On connection to socket -> add the socket to the correct room
+  
+  server.listen(3000, () => {
+    console.log('listening on *:3000');
+  });
